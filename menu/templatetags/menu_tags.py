f3f0e5fda40eba53daac49571208c518ffd7b7ec
Makterro <makkrs@mail.ru>
2025-11-12 11:57:02 +0700

    feat(templatetag): implement draw_menu inclusion tag

---
+++ menu/templatetags/menu_tags.py
@@ -0,0 +1,23 @@
+from django import template
+from menu.models import MenuItem, Menu
+
+register = template.Library()
+
+@register.inclusion_tag('menu/draw_menu.html', takes_context=True)
+def draw_menu(context, menu_name):
+    request = context['request']
+    current_path = request.path
+
+    menu = Menu.objects.prefetch_related('items__children').get(name=menu_name)
+    items = list(menu.items.all())
+
+    active_item = next((i for i in items if i.get_url() == current_path), None)
+
+    def build_tree(parent=None):
+        return [dict(
+            item=item,
+            children=build_tree(item)
+        ) for item in items if item.parent == parent]
+
+    tree = build_tree()
+    return {'menu_tree': tree, 'active_item': active_item, 'current_path': current_path}
