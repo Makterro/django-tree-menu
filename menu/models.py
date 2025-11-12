22cb424da387fbf1961791b4667b6dc5fd2d2322
Makterro <makkrs@mail.ru>
2025-11-12 11:54:20 +0700

    feat(admin): add and register Menu, MenuItem with inline editing

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(Menu, related_name='items', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=255, blank=True, null=True)
+    named_url = models.CharField(max_length=100, blank=True, null=True)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        if self.named_url:
+            try:
+                return reverse(self.named_url)
+            except:
+                return '#'
+        return self.url or '#'
